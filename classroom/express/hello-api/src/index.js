4ab95dcea63b33f322bdd3dc12be067f5ebf67ca
Luiz Chaves <lucachaves@gmail.com>
2025-05-26 12:37:02 -0300

    fix: doc hello-api

--- classroom/express/hello-api/src/index.js
+++ classroom/express/hello-api/src/index.js
@@ -3,6 +3,8 @@ import morgan from 'morgan';
 
 const server = express();
 
+server.use(express.json()); // Middleware to parse JSON bodies
+
 server.use(morgan('tiny'));
 
 // route = Method (get, post, put, delete), Path, Handler
@@ -10,6 +12,9 @@ server.get('/hello', (req, res) => {
   return res.send('Hello World!');
 });
 
+// query params/search params/query string
+// url?param1=value1&param2=value2
+// req.query
 // get /hello/pt
 server.get('/hello/pt', (req, res) => {
   // const name = req.query.name;
@@ -26,6 +31,9 @@ server.get('/hello/pt', (req, res) => {
   });
 });
 
+// router param
+// req.params
+// get /hello/en/:name/:age
 // get /hello/en
 server.get('/hello/en/:name', (req, res) => {
   // const name = req.params.name;
@@ -42,7 +50,22 @@ server.get('/hello/en/:name', (req, res) => {
   });
 });
 
+// body params
 // post /hello/es
+server.post('/hello/es', (req, res) => {
+  // const name = req.body.name;
+  const { name } = req.body // JSON
+
+  if (!name) {
+    return res.status(400).json({
+      error: 'Name is required',
+    });
+  }
+
+  return res.json({
+    message: `Hola, ${name}!`,
+  });
+});
 
 // 404 handler
 server.use((req, res, next) => {
